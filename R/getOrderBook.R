66850e9c821ae88984e78d4ff918714b19cb3c57
retooooo <reto.led@gmail.com>
2021-02-11 00:09:29 +0100

    fiexed getOrderbook

--- R/getOrderBook.R
+++ R/getOrderBook.R
@@ -4,13 +4,23 @@ getOrderBook <- function(symbol = "BTC", symbol_base = "USDT", limit = 1000) {
     stop(str_glue("{symbol}{symbol_base} is not a valid symbol.")) 
   }
   ##TODO: write cpp version of this lapply
-  limit <- length(tmp$bids)
-  lapply(1:limit, function(i) {
+  bids <- lapply(1:length(tmp$bids), function(i) {
     data.frame(
-      TYPE = c("BID", "ASK"),
-      QUANTITY = as.numeric(c(tmp$bids[[i]][[2]], tmp$asks[[i]][[2]])),
-      PRICE = as.numeric(c(tmp$bids[[i]][[1]], tmp$asks[[i]][[1]]))
+      TYPE = "BID",
+      QUANTITY = as.numeric(tmp$bids[[i]][[2]]),
+      PRICE = as.numeric(tmp$bids[[i]][[1]])
     )
   }) %>%
     bind_rows()
+  
+  asks <- lapply(1:length(tmp$asks), function(i) {
+    data.frame(
+      TYPE = "ASK",
+      QUANTITY = as.numeric(tmp$asks[[i]][[2]]),
+      PRICE = as.numeric(tmp$asks[[i]][[1]])
+    )
+  }) %>%
+    bind_rows()
+  
+  bind_rows(bids, asks)
 }
